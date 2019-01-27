affb442f7d38668da84923d8cfe2e6740efebbfe
Brendan Van Allen <brendan.vanallen1@marist.edu>
2019-01-27 16:23:18 -0500

    Added project files

---
+++ Lab0/src/HelloAlan.java
@@ -0,0 +1,9 @@
+/**
+ * This class serves as a test to ensure you can properly
+ * run my projects.
+ */
+public class HelloAlan {
+    public static void main(String args[]) {
+        System.out.println("Hello Alan!");
+    }
+}
