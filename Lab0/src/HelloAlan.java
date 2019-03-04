064c46e4e21911add4019ffbb8cf786e03971998
Brendan Van Allen <brendan.vanallen1@marist.edu>
2019-03-04 18:27:15 -0500

    Restructing w/ branches for projects

--- Lab0/src/HelloAlan.java
+++
@@ -1,9 +0,0 @@
-/**
- * This class serves as a test to ensure you can properly
- * run my projects.
- */
-public class HelloAlan {
-    public static void main(String args[]) {
-        System.out.println("Hello Alan!");
-    }
-}
